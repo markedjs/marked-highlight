4a1f6d6a890cab8778ef6cdb1f18d9d60eba1f5c
Samuel Littley <samuel.littley@toastwaffle.com>
2023-05-25 22:51:56 +0100

    test: Add tests for type declarations using tsd (#35)

---
+++ types_test/index.test-d.ts
@@ -0,0 +1,48 @@
+import {markedHighlight} from 'marked-highlight';
+import {expectError} from 'tsd';
+
+// Single function argument
+markedHighlight((code: string, language: string) => code+language);
+
+// Minimal synchronous options argument
+markedHighlight({
+  highlight(code: string, language: string) {
+    return code+language;
+  }
+});
+
+// Full synchronous options argument
+markedHighlight({
+  highlight(code: string, language: string) {
+    return code+language;
+  },
+  async: false,
+  langPrefix: 'foo',
+});
+
+// Force asynchronous extension with synchronous highlight function
+markedHighlight({
+  highlight(code: string, language: string) {
+    return code+language;
+  },
+  async: false,
+  langPrefix: 'foo',
+});
+
+// Minimal asynchronous options argument
+markedHighlight({
+  highlight: async (code: string, language: string) => code+language,
+  async: true,
+})
+
+// Full asynchronous options argument
+markedHighlight({
+  highlight: async (code: string, language: string) => code+language,
+  async: true,
+  langPrefix: 'foo',
+})
+
+// Invalid asynchronous options argument - missing async: true
+expectError(markedHighlight({
+  highlight: async (code: string, language: string) => code+language,
+}));
